4333c07338b3f24f9937b065cced0e193d7ff439
최정필 <wjdvlf1407@gmail.com>
2022-04-14 23:05:46 +0900

    1.130 Ex13 문제풀이 시작

---
+++ src/com/javaex/practice/Ex13.java
@@ -0,0 +1,17 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex13 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("환전할 원화를 입력하세요: ");
+		
+		sc.close();
+		
+	}
+
+}
