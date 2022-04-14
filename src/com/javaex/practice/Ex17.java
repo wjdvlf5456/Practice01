cb714f11d71e3e6f5e904eb390b008d410262a45
최정필 <wjdvlf1407@gmail.com>
2022-04-14 23:39:57 +0900

    1.71 Ex17 문제풀이 정수형변환 안 해서 오류

---
+++ src/com/javaex/practice/Ex17.java
@@ -0,0 +1,24 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex17 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("반지름: ");
+		double radius = sc.nextDouble();
+		
+		double pi = 3.14;
+		double vol = 4/3*pi*radius*radius*radius;
+		
+		System.out.println("구의 부피는: "+vol+"입니다.");
+		
+		sc.close();
+		
+		
+	}
+
+}
