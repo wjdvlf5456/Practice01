b0cd6f0530c0d9ac325fbc95a025cebf43252fa9
최정필 <wjdvlf1407@gmail.com>
2022-04-14 22:08:55 +0900

    1.041 Ex04 풀이

---
+++ src/com/javaex/practice/Ex04.java
@@ -0,0 +1,29 @@
+package com.javaex.practice;
+
+public class Ex04 {
+
+	public static void main(String[] args) {
+		/*Ex04 예
+		int x,y --> 하나의 변수만 사용한다. 
+		
+		x = 10	--> 둘 다 맞는 예약어를 앞에 샤용하여 구분해주어야 한다.
+		y = 20 
+		
+		sum = x+y --> 예약어 미입력   
+		System.out.println("합은 ' +sum);  문장을 넣고 싶을 때는 "를 사용하여야 한다. 
+		*/
+		
+		
+		//정답
+		
+		int x = 10;
+		int y = 20;
+		
+		int sum = x+y;
+		
+		System.out.println("합은 " +sum);
+		
+		
+	}
+
+}
