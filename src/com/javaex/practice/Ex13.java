4348dc9c9c917c5cdd0bb30b244fdcd1b552c30b
최정필 <wjdvlf1407@gmail.com>
2022-04-14 23:09:59 +0900

    1.131 Ex13 문제풀이

--- src/com/javaex/practice/Ex13.java
+++ src/com/javaex/practice/Ex13.java
@@ -9,6 +9,10 @@ public class Ex13 {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("환전할 원화를 입력하세요: ");
+		double won = sc.nextDouble();
+		
+		double dollar = won/1230.95;
+		System.out.println("받으실 달러는 "+dollar+"");
 		
 		sc.close();
 		
