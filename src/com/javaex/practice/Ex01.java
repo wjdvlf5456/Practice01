0119f054f5d1f246c9cc3771d2b58f510e99845a
최정필 <wjdvlf1407@gmail.com>
2022-04-14 18:17:52 +0900

    2022-04-14 과제(자바) 1.0

---
+++ src/com/javaex/practice/Ex01.java
@@ -0,0 +1,18 @@
+package com.javaex.practice;
+
+public class Ex01 {
+	
+	public static void main(String[] args) {
+		
+		/*
+		int true;
+		int #_of_workers;
+		int countOfLettersLnString;
+		int 1stLevel1;
+		int person#;
+		int _person;
+		*/
+		
+	}
+
+}
---
+++ src/com/javaex/practice/Ex02.java
@@ -0,0 +1,19 @@
+package com.javaex.practice;
+
+public class Ex02 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("===========================");
+		System.out.println( 12 / 5-3);
+		
+		
+		
+		
+		
+		System.out.println("===========================");
+		System.out.println("2번 문제");
+		System.out.println( 5 + 19 % 3 );
+	}
+
+}
