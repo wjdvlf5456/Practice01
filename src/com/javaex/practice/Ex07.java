eb058cae160d087860d42d9c4c40273c0ff7a536
최정필 <wjdvlf1407@gmail.com>
2022-04-14 22:25:07 +0900

    1.07 Ex07 문제풀이

--- src/com/javaex/practice/Ex07.java
+++ src/com/javaex/practice/Ex07.java
@@ -8,9 +8,14 @@ public class Ex07 {
 		int n = ++i %2;			//i앞에 ++가 와있으므로 10+1=11 --> 11%2는 1이므로 int n 은 1이다. 
 		
 		//예상 i= 10, n= 1
-		//정답 i=
+		//정답 i= 11  n= 1
 		System.out.println(i);
 		System.out.println(n);
+		
+		//int n 에서 i는 11이 되었으므로 출력값은 11이다.
+		
+		
+		//Ex06과 다르게 i앞에 ++이 오므로 n의 값이 바뀌었다.
 
 	}
 
