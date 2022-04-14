e86779aaaa70ebaa9cae62458c13e816d547d905
최정필 <wjdvlf1407@gmail.com>
2022-04-14 22:55:55 +0900

    1.11 Ex11 문제풀이

---
+++ src/com/javaex/practice/Ex11.java
@@ -0,0 +1,23 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex11 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("월급을 입력하세요:");
+		int salary = sc.nextInt();
+		int saving = salary*12*10;
+		System.out.println("10년동안 최대 저축액은 "+saving+"원 입니다.");
+		
+		
+		
+		sc.close();
+		
+		
+	}
+
+}
